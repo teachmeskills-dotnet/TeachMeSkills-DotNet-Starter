5fd5da935bd5ad85ec1e582ef274b26f8b694bd7
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-09-15 01:43:58 +0300

    init: add basic template with tasks

---
+++ src/TeachMeSkills.DotNet.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code here..
+
+            Console.ReadLine();
+        }
+    }
+}
